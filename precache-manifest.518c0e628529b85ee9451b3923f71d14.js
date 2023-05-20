07306a9edcc84612cc3bc944f990133d69a82810
15Santiago <santiagorodriy@gmail.com>
2023-05-20 15:11:28 -0600

    Updates

---
+++ precache-manifest.518c0e628529b85ee9451b3923f71d14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77a70a045f9c1c996687",
+    "url": "/15Santiago/proyecto-integrador-react/static/css/main.4409fee5.chunk.css"
+  },
+  {
+    "revision": "77a70a045f9c1c996687",
+    "url": "/15Santiago/proyecto-integrador-react/static/js/main.77a70a04.chunk.js"
+  },
+  {
+    "revision": "300d06035c11cb6f83db",
+    "url": "/15Santiago/proyecto-integrador-react/static/js/1.300d0603.chunk.js"
+  },
+  {
+    "revision": "f2b6193381bfb0078b3e",
+    "url": "/15Santiago/proyecto-integrador-react/static/js/runtime~main.f2b61933.js"
+  },
+  {
+    "revision": "e10e460cefbb8445f9fde78b4f8e8e0a",
+    "url": "/15Santiago/proyecto-integrador-react/index.html"
+  }
+];
